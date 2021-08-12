321e569b2882c31b0346699304f5b222681cb082
Starman <30315137+Starman3787@users.noreply.github.com>
2021-08-12 07:51:18 +0100

    Update fetchFile.js

--- src/fetchFile.js
+++ src/fetchFile.js
@@ -21,16 +21,24 @@ function fetchFile(url, guild_id, channel_id, attachment_id, file_size) {
         if (path && existsSync(path)) {
             /* here we can just do what we did before when downloading the file, but in reverse */
             /* first we'll create a stream, which reads the content of the file that we've found */
-            const stream = createReadStream(path)
-                /* now we can decrypt the file */
-                /* the key and iv must be recalculated using the IDs connected with the file, similar to before */
-                /* if we don't have the correct info, we cannot decrypt the file */
-                .pipe(createDecipheriv("aes-256-cbc", hash.sha512().update(`${hash.sha512().update(`${guild_id}${channel_id}${attachment_id}${file_size}`).digest("hex")}${url}`).digest("hex").slice(0, 32), hash.sha512().update(`${hash.sha512().update(`${guild_id}${channel_id}${attachment_id}`).digest("hex")}${file_size}`).digest("hex").slice(0, 16)))
-                /* next we can decompress the file */
-                .pipe(createGunzip());
-            /* we'll return the path of the file (so it can be deleted once we're done with it) */
-            /* and we'll also return the read stream to the file, which is used to upload the file to the serverlog */
-            return resolve({ path, stream });
+            try {
+                const stream = createReadStream(path)
+                    /* now we can decrypt the file */
+                    /* the key and iv must be recalculated using the IDs connected with the file, similar to before */
+                    /* if we don't have the correct info, we cannot decrypt the file */
+                    .pipe(createDecipheriv("aes-256-cbc", hash.sha512().update(`${hash.sha512().update(`${guild_id}${channel_id}${attachment_id}${file_size}`).digest("hex")}${url}`).digest("hex").slice(0, 32), hash.sha512().update(`${hash.sha512().update(`${guild_id}${channel_id}${attachment_id}`).digest("hex")}${file_size}`).digest("hex").slice(0, 16)))
+                    /* next we can decompress the file */
+                    .pipe(createGunzip());
+                /* we'll return the path of the file (so it can be deleted once we're done with it) */
+                /* and we'll also return the read stream to the file, which is used to upload the file to the serverlog */
+                return resolve({ path, stream });
+            } catch (_) {
+                /* if we encounter an error here, it's usually down to some issue with the decryption */
+                /* we should catch the error so it doesn't crash everything */
+                /* the fact that i've not been able to replicate this issue for over a year, nor has anyone reported it, shows how minor it is */
+                /* so should be fine just to let it fail */
+                return reject();
+            }
         }
         else
             return reject(new Error("File does not exist"));
