b90a4ed120a2ba4b72bb31b93794b77ce1f0ee7e
Starman <30315137+Starman3787@users.noreply.github.com>
2023-04-19 07:56:15 +0100

    update max file size in line with discord's update

--- src/checkMaxAttachmentSize.js
+++ src/checkMaxAttachmentSize.js
@@ -2,7 +2,7 @@ function checkMaxAttachmentSize(premium_tier) {
     switch (premium_tier) {
         case 2: return 52420000;
         case 3: return 104840000;
-        default: return 8380000;
+        default: return 26110000;
     }
 }
 
